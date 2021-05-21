73b86e4ceb070fdeb642ec7a439954fe7e3df202
Walison Fernandes <walisonfbalves.ti@gmail.com>
2021-05-21 13:15:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.querySelector()
+function cadastrarCliente(){
+    var nome = document.getElementById("nome")
+    var rg = document.getElementById("RG")
+    var cpf = document.getElementById("CPF")
+    var agencia = document.getElementById("agencia")
+    var conta = document.getElementById("conta")
+
+    var dados = localStorage.getItem("dadosCliente");
+    
+    if(dados == null){
+        localStorage.setItem("dadosCliente", "[]");
+        dados = [];
+    
+    }
+}
