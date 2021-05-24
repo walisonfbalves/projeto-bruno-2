d6710671804905f7d4929ba8120a7b37929917f8
Walison Fernandes <walisonfbalves.ti@gmail.com>
2021-05-23 23:35:31 -0300

    Update
    Falhando muito, mas progredindo

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
-function cadastrarCliente(){
-    var nome = document.getElementById("nome")
-    var rg = document.getElementById("RG")
-    var cpf = document.getElementById("CPF")
-    var agencia = document.getElementById("agencia")
-    var conta = document.getElementById("conta")
+function inserirCliente(nome, rg, cpf, agencia, conta){
+    
+    //peguei a tabela pelo id
+    var clientes = document.getElementById("tbCliente");
 
-    var dados = localStorage.getItem("dadosCliente");
-    
-    if(dados == null){
-        localStorage.setItem("dadosCliente", "[]");
-        dados = [];
-    
+    // ver quantas linhas tem a tabela
+    var qtdLinhas = clientes.rows.length;
+
+    // Inserindo uma linha, e depois da ultima linha inserir uma nova
+    var linha = clientes.insertRow(qtdLinhas);
+
+    // Inserindo as colunas
+    var cellID = linha.insertCell(0);
+    var cellNome = linha.insertCell(1);
+    var cellRG = linha.insertCell(2);
+    var cellCPF = linha.insertCell(3);
+    var cellAgencia = linha.insertCell(4);
+    var cellConta = linha.insertCell(5);
+
+    cellID.innerHTML = qtdLinhas;
+    cellNome.innerHTML = nome;
+    cellRG.innerHTML = rg;
+    cellCPF.innerHTML = cpf;
+    cellAgencia.innerHTML = agencia;
+    cellConta.innerHTML = conta;
+
+    preencherCamposForm();
+}
+
+function salvarCliente(nome, rg, cpf, agencia, conta) {
+
+    // função para poder alterar os clientes
+    clientes.rows[index].cells[1].innerHTML = nome;
+    clientes.rows[index].cells[2].innerHTML = rg;
+    clientes.rows[index].cells[3].innerHTML = cpf;
+    clientes.rows[index].cells[4].innerHTML = agencia;
+    clientes.rows[index].cells[5].innerHTML = conta;
+
+}
+
+function preencherCamposForm() {
+
+    for(var i = 0; i < clientes.rows.length; i++)
+    {
+        clientes.rows[i].onclick = function()
+        {
+            index = this.rowIndex;
+            document.getElementById("ID").value = pessoas.rows[index].cells[0].innerText;
+            document.getElementById("nome").value = pessoas.rows[index].cells[1].innerText;
+            document.getElementById("RG").value = pessoas.rows[index].cells[2].innerText;
+            document.getElementById("CPF").value = pessoas.rows[index].cells[3].innerText;
+            document.getElementById("agencia").value = pessoas.rows[index].cells[4].innerText;
+            document.getElementById("conta").value = pessoas.rows[index].cells[5].innerText;
+        }
     }
 }
+
+function excluirCliente() {
+
+    for(var i = 0; i < clientes.rows.length; i++)
+    {
+        if (index == i)
+            clientes.deleteRow(index);
+            return;
+    }
+}
+    
---
+++ script1.js
@@ -0,0 +1,109 @@
+class Cliente {
+
+    constructor() 
+    {
+        this.id = 1;
+        this.arrayClientes = [];
+    }
+
+    salvar() 
+    {
+
+        let cliente = this.lerDados();
+
+        if(this.validaCampos(cliente)) {
+            this.adicionar(cliente);
+        }
+
+        this.listaTabela();
+        
+    }
+
+    listaTabela() 
+
+    {
+
+        let tbody = document.getElementById("tbody")
+        
+        for(let i = 0; i < this.arrayClientes.length; i++) {
+
+            tbody.innerText = "";
+
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertRow();
+            let td_nome = tr.insertRow();
+            let td_rg = tr.insertRow();
+            let td_cpf = tr.insertRow();
+            let td_agencia = tr.insertRow();
+            let td_conta = tr.insertRow();
+            let td_acoes = tr.insertRow();
+
+            td_id.innerText = this.arrayClientes[i].id;
+            td_nome.innerText = this.arrayClientes[i].nome;
+            td_rg.innerText = this.arrayClientes[i].rg;
+            td_cpf.innerText = this.arrayClientes[i].cpf;
+            td_agencia.innerText = this.arrayClientes[i].agencia;
+            td_conta.innerText = this.arrayClientes[i].conta;
+            td_acoes.innerText = this.arrayClientes[i].acoes;
+        }
+    }
+
+    adicionar(cliente)
+    {
+        this.arrayClientes.push(cliente);
+        this.id++;
+    }
+
+
+    lerDados() 
+    {
+        let cliente = {}
+
+        cliente.id = this.id;
+        cliente.nome = document.getElementById('nome').value;
+        cliente.rg = document.getElementById('rg').value;
+        cliente.cpf = document.getElementById('cpf').value;
+        cliente.agencia = document.getElementById('agencia').value;
+        cliente.conta = document.getElementById('Conta').value;
+
+        return cliente;
+    }
+
+    validaCampos(cliente) 
+    {
+        let msg = "";
+
+        if(cliente.nome == '') {
+            msg += " - Informe o nome do cliente \n";
+        }
+
+        if(cliente.rg == '') {
+            msg += " - Informe o RG do cliente \n";
+        }
+
+        if(cliente.cpf == '') {
+            msg += " - Informe o CPF do cliente \n";
+        }
+
+        if(cliente.agencia == '') {
+            msg += " - Informe a agência do cliente \n";
+        }
+
+        if(cliente.conta == '') {
+            msg += " - Informe a conta do cliente \n";
+        }
+        
+    }
+
+    cancelar() 
+    {
+        document.getElementById('nome').value = "";
+        document.getElementById('rg').value = "";
+        document.getElementById('cpf').value = "";
+        document.getElementById('agencia').value = "";
+        document.getElementById('conta').value = "";
+    }
+}
+
+    var cliente = new Cliente();
